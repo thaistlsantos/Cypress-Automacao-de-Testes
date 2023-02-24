feaeca9b23ebc403682055380a07b9079c6d31c2
Thaís de Lima Santos <67654045+thaistlsantos@users.noreply.github.com>
2023-02-24 18:48:39 -0300

    Subindo arquivos

--- Projeto01/cypress.config.js
+++ Projeto01/cypress.config.js
@@ -5,5 +5,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/report/mochawesome-report",
+      overwrite: true,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss",
+    },
   },
 });
+
+
---
+++ Projeto01/cypress/e2e/3-alurapic/alura-busca.cy.js
@@ -0,0 +1,32 @@
+describe('alura busca cursos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://www.alura.com.br');
+    })
+
+    // arrange = preparação do ambiente
+    // assert = o que quero verificar
+    // act = ação que desejo fazer
+
+
+    it('buscar curso de java', () => {
+        cy.get('#header-barraBusca-form-campoBusca').type('java');
+        cy.get('.header-barraBusca-form-submit').click();
+        //cy.get(':nth-child(5) > .busca-resultado-link > .busca-resultado-container > .busca-resultado-nome').should('have.text', 'Formação Java e Orientação a Objetos');
+        cy.get('h4.busca-resultado-nome').should('contain', 'Formação Java e Orientação a Objetos');
+    })
+
+    it('', () => {
+        
+    });
+    
+
+    it('', () => {
+        
+    });
+
+    
+
+
+
+})
