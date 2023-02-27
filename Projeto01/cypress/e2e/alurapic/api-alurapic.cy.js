4a0eef9c1bd1648d4303ced94fe70453afe0fcd8
Thaís de Lima Santos <67654045+thaistlsantos@users.noreply.github.com>
2023-02-27 18:58:29 -0300

    Update project

--- Projeto01/cypress/e2e/alurapic/api-alurapic.cy.js
+++ Projeto01/cypress/e2e/alurapic/api-alurapic.cy.js
@@ -1,14 +1,16 @@
-describe('Buscar fotos e dados', ()=> {
+const { expect } = require("chai")
 
-    it('buscar fotos do flavio', ()=> {
+
+describe("Buscar fotos e dados", () => {
+    it("Buscar fotos do Flávio", () => {
         cy.request({
-            method: 'GET', 
-            url: 'https://apialurapic.herokuapp.com/flavio/photos'
+            method: "GET",
+            url: "https://apialurapic.herokuapp.com/flavio/photos",
         }).then((res) => {
-            expect(res.status).to.be.equal(200)
-            
-        }
-
-        )
-    })
-})
+            expect(res.status).to.be.equal(200); // Verificar a chamada da request
+            expect(res.body).is.not.empty; // Verificar se o corpo da resposta tem informações
+            expect(res.body[0]).to.have.property("description"); // Verificar se contem  texto 'Farol iluminado'
+            expect(res.body[0].description).to.be.equal("Farol iluminado");
+        });
+    });
+});
